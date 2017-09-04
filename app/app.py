1fcc9bf4cf9c7e68356a0aeec17377a9af70368e
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-09-04 21:53:24 +0200

    Adding draft for upload mechanism

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for, jsonify, request
+
+import os
+from flask import Flask, render_template, url_for, jsonify, request, redirect
+from werkzeug.utils import secure_filename
 from glob import glob
 import json
 app = Flask(__name__)
+
+
 
 #########################
 #  Main pages           #
@@ -47,6 +52,54 @@ def podcast(name):
     response = jsonify(data)
     response.status_code = 200
     return response
+
+
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 #########################
 #  Static stuff         #
