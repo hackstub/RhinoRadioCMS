75546142b7cd2117da3bb60df91bb003e732c5f9
Jeremie W <jeremie@la-oui.ch>
2017-09-24 19:43:33 +0200

    Tried resolving a very weird bug on DB declaration

--- app/models.py
+++ app/models.py
@@ -2,45 +2,54 @@ from . import db, admin
 from datetime import datetime
 from flask_admin.contrib.sqla import ModelView
 
-# class Publication(db.Model):
-#     __tablename__ = 'pubs'
-#     id = db.Column(db.Integer)
-#     podcast_id = db.Column(db.Integer, db.ForeignKey('podcasts.id'), primary_key=True)
-# #     section_id = db.Column(db.Integer, db.ForeignKey('sections.id'), nullable=True)
-#     author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), primary_key=True)
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-# 
-#     
-# 
-# 
-# class Podcast(db.Model):
-#     __tablename__ = 'podcasts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(512), unique=True)
-#     authors = db.relationship('Publication', foreign_keys=[Publication.author_id], backref=db.backref('podcasts', lazy="select"), lazy="join", cascade='all,delete-orphan')
-#     desc = db.Column(db.Text)
-#     label = db.Column(db.String(128))
-#     date = db.Column(db.Date)
-#     mood = db.Column(db.SmallInteger)    
-#     link = db.Column(db.String(256))
+class Publication(db.Model):
+    __tablename__ = 'publications'
+    id = db.Column(db.Integer)
+    podcast_id = db.Column(db.Integer, db.ForeignKey('podcasts.id'),
+                            primary_key=True)
+#     section_id = db.Column(db.Integer,
+#                             db.ForeignKey('sections.id'),
+#                             nullable=True)
+    author_id = db.Column(db.Integer,
+                            db.ForeignKey('authors.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Podcast(db.Model):
+    __tablename__ = 'podcasts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(512), unique=True)
+    authors = db.relationship('Publication',
+                                foreign_keys=[Publication.author_id],
+                                backref=db.backref('podcasts', lazy='select'),
+                                lazy='join',
+                                cascade='all, delete-orphan')
+    desc = db.Column(db.Text)
+    label = db.Column(db.String(128))
+    date = db.Column(db.Date)
+    mood = db.Column(db.SmallInteger)    
+    link = db.Column(db.String(256))
 #     type = db.Column(db.Boolean())
-#     tags = db.Column(db.String(128))
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-#     
-#     def name():
-#         return name
-#     
-#     def link():
-#         return link
-#         
-# 
-# class Author(db.Model):
-#     __tablename__ = 'authors'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128))
-#     publications = db.relationship('Publication', foreign_keys=[Publication.id], backref=db.backref('authors', lazy="join"), lazy="select", cascade='all,delete-orphan')
-#     status = db.Column(db.String(128))
-#     bio = db.Column(db.Text)
+    tags = db.Column(db.String(128))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def name():
+        return name
+    
+    def link():
+        return link        
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    publications = db.relationship('Publication',
+                                    foreign_keys=[Publication.id],
+                                    backref=db.backref('authors', lazy='join'),
+                                    lazy='select',
+                                    cascade='all, delete-orphan')
+    status = db.Column(db.String(128))
+    bio = db.Column(db.Text)
 
 class Follow(db.Model):
     __tablename__ = 'follows'
