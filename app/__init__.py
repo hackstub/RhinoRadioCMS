9832f4552cd5dbc9188de47758d008b38e06de8d
mandlkind <jeremiew@mailoo.org>
2017-09-25 15:20:47 +0200

    Small upgrades for the model integration

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_mail import Mail
 from flask_admin import Admin
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 from glob import glob
 from config import config
 
