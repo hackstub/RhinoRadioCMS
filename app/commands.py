44bacb8e9480e02620c9993a7bb74a020c3c6e50
Axolotle <nchesnais@mailoo.org>
2017-11-15 23:35:22 +0100

    adding a function to drop all cascade (NEED TO BE TESTED)

--- app/commands.py
+++ app/commands.py
@@ -54,6 +54,54 @@ class NukeCommand(Command):
         self.db.session.commit()
         print("Session committed\n")
 
+    def a_tester_pour_niquer_les_cascades(self):
+        from sqlalchemy.engine import reflection
+        from sqlalchemy import create_engine
+        from sqlalchemy.schema import (
+            MetaData,
+            Table,
+            DropTable,
+            ForeignKeyConstraint,
+            DropConstraint,
+            )
+        # From http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+
+        conn=db.engine.connect()
+
+        # the transaction only applies if the DB supports
+        # transactional DDL, i.e. Postgresql, MS SQL Server
+        trans = conn.begin()
+
+        inspector = reflection.Inspector.from_engine(db.engine)
+
+        # gather all data first before dropping anything.
+        # some DBs lock after things have been dropped in
+        # a transaction.
+        metadata = MetaData()
+
+        tbs = []
+        all_fks = []
+
+        for table_name in inspector.get_table_names():
+            fks = []
+            for fk in inspector.get_foreign_keys(table_name):
+                if not fk['name']:
+                    continue
+                fks.append(
+                    ForeignKeyConstraint((),(),name=fk['name'])
+                    )
+            t = Table(table_name,metadata,*fks)
+            tbs.append(t)
+            all_fks.extend(fks)
+
+        for fkc in all_fks:
+            conn.execute(DropConstraint(fkc))
+
+        for table in tbs:
+            conn.execute(DropTable(table))
+
+        trans.commit()
+
 class LoremCommand(Command):
     """Feed database with placeholders"""
 
